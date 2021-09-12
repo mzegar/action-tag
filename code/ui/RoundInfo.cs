f566e6c990334f7ee3b8876a3df22cc576e17532
Matthew Zegar <matthewzegar@gmail.com>
2021-09-12 15:57:12 -0700

    Finialize round info hud

--- code/ui/RoundInfo.cs
+++ code/ui/RoundInfo.cs
@@ -1,12 +1,11 @@
-﻿using Sandbox.UI;
+﻿using Sandbox;
+using Sandbox.UI;
 using Sandbox.UI.Construct;
 
 namespace ActionTag
 {
 	public class RoundInfo : Panel
 	{
-		private readonly Sandbox.UI.Panel _timeWrapper;
-		
 		private readonly Label _currentRound;
 		private readonly Label _currentTime;
 		
@@ -17,8 +16,8 @@ namespace ActionTag
 			var roundWrapper = Add.Panel( "Round" );
 			_currentRound = roundWrapper.Add.Label( "" );
 
-			_timeWrapper = Add.Panel( "Time" );
-			_currentTime = _timeWrapper.Add.Label( "" );
+			var timerWrapper = Add.Panel( "Timer" );
+			_currentTime = timerWrapper.Add.Label( "" );
 		}
 
 		public override void Tick()
@@ -26,9 +25,15 @@ namespace ActionTag
 			base.Tick();
 
 			_currentRound.Text = ActionTagGame.Instance?.Round.RoundName;
-			
-			_timeWrapper.SetClass("disabled", ActionTagGame.Instance?.Round.RoundDuration == 0);
-			_currentTime.Text = ActionTagGame.Instance?.Round.TimeLeftFormatted;
+
+			if ( ActionTagGame.Instance?.Round is not WaitingRound )
+			{
+				_currentTime.Text = $"⏱️ {ActionTagGame.Instance?.Round.TimeLeftFormatted}";
+			}
+			else
+			{
+				_currentTime.Text = $"{Client.All.Count} / {ActionTagGameSettings.MinimumPlayers}";
+			}
 		}
 	}
 }
