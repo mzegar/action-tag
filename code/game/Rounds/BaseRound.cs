c85d93dbabb1d9f6b927e4af5de558ee4c091654
Matthew Zegar <matthewzegar@gmail.com>
2021-09-27 07:35:42 -0700

    Change to base networkable

--- code/game/Rounds/BaseRound.cs
+++ code/game/Rounds/BaseRound.cs
@@ -4,7 +4,7 @@ using Sandbox;
 
 namespace ActionTag
 {
-    public abstract partial class BaseRound : NetworkComponent
+    public abstract partial class BaseRound : BaseNetworkable
     {
 	    public virtual int RoundDuration => 0;
         public virtual string RoundName => "";
