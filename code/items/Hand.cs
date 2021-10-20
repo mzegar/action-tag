39d5dc5e5f4f7e4d6586fe1ad83a936a4b25a53f
Matthew Zegar <matthewzegar@gmail.com>
2021-10-19 22:05:25 -0700

    Increase tag distance

--- code/items/Hand.cs
+++ code/items/Hand.cs
@@ -11,19 +11,19 @@ namespace ActionTag
 		public override float SecondaryRate => 0.3f;
 		public override bool IsMelee => true;
 		public override int BaseDamage => 10000;
-		public virtual int MeleeDistance => 40;
+		public virtual int MeleeDistance => 55;
 
 		public override void Spawn()
 		{
 			base.Spawn();
-			
+
 			SetModel( "" );
 		}
 
 		private async Task DelayStrike()
 		{
 			await Task.Delay( 300 );
-			MeleeStrike(BaseDamage, 5f);
+			MeleeStrike( BaseDamage, 5f );
 		}
 
 		private void MeleeStrike( float damage, float force )
@@ -32,25 +32,25 @@ namespace ActionTag
 			{
 				return;
 			}
-			
+
 			var forward = Owner.EyeRot.Forward;
 			forward = forward.Normal;
 
 			foreach ( var tr in TraceBullet( Owner.EyePos, Owner.EyePos + forward * MeleeDistance, 10f ) )
 			{
 				if ( !IsValid ) return;
-				
+
 				if ( !tr.Entity.IsValid() ) continue;
 
 				if ( !IsServer ) continue;
-				
+
 				if ( ActionTagGame.Instance?.Round is not PlayRound ) continue;
 
 				if ( Owner is not ActionTagPlayer player || tr.Entity is not ActionTagPlayer otherPlayer ) return;
 
 				using ( Prediction.Off() )
 				{
-					switch (player.Team)
+					switch ( player.Team )
 					{
 						case ChasersTeam when otherPlayer.Team is RunnerTeam:
 							otherPlayer.Controller.IsFrozen = true;
@@ -70,7 +70,7 @@ namespace ActionTag
 			ShootEffects();
 			_ = DelayStrike();
 		}
-		
+
 		public override void SimulateAnimator( PawnAnimator anim )
 		{
 			anim.SetParam( "holdtype", 4 );
