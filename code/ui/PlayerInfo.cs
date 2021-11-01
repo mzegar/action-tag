b56dab709d459c9a7a34d0e7441c2b5a0d038ede
Matthew Zegar <matthewzegar@gmail.com>
2021-11-01 15:53:17 -0700

    Remove health

--- code/ui/PlayerInfo.cs
+++ code/ui/PlayerInfo.cs
@@ -11,40 +11,27 @@ namespace ActionTag
 		public static PlayerInfo Instance;
 		private readonly Sandbox.UI.Panel _teamWrapper;
 		private readonly Label _currentTeam;
-		private readonly Label _currentHealth;
-		
+
 		public PlayerInfo()
 		{
 			Instance = this;
-			StyleSheet.Load("/ui/PlayerInfo.scss");
-			
+			StyleSheet.Load( "/ui/PlayerInfo.scss" );
+
 			var team = "";
-			var health = "";
 
 			if ( Local.Pawn is ActionTagPlayer player )
 			{
 				team = player.Team.Name;
-				health = $"✚ {player.Health.CeilToInt()}";
 			}
 
 			_teamWrapper = Add.Panel( "Team" );
-			_teamWrapper.SetClass("disabled", team is "" or "None"); // TODO: Clean this up when spectating players is a thing.
+			_teamWrapper.SetClass( "disabled", team is "" or "None" ); // TODO: Clean this up when spectating players is a thing.
 			_currentTeam = _teamWrapper.Add.Label( team );
-
-			var healthWrapper = Add.Panel( "Health" );
-			_currentHealth = healthWrapper.Add.Label( health );
 		}
 
 		public override void Tick()
 		{
 			base.Tick();
-			
-			if (Local.Pawn is not ActionTagPlayer player)
-			{
-				return;
-			}
-
-			_currentHealth.Text = $"✚ {player.Health.CeilToInt()}";
 		}
 
 		public void UpdateCurrentTeam()
@@ -53,8 +40,8 @@ namespace ActionTag
 			{
 				return;
 			}
-			
-			_teamWrapper.SetClass("disabled", player.Team is null or NoneTeam);
+
+			_teamWrapper.SetClass( "disabled", player.Team is null or NoneTeam );
 			_currentTeam.Text = player.Team?.Name;
 		}
 	}
